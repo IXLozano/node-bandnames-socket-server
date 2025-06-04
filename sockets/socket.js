9aa79d3576bbbc82619bc9de219ad8c4e99be1a2
Isaac <ixlozano30@gmail.com>
2025-06-03 18:45:28 -0600

    End of section 3

---
+++ sockets/socket.js
@@ -0,0 +1,16 @@
+// Socket messages
+const {io} = require('../index');
+io.on('connection', client => {
+    console.log('Client connected');
+
+
+    client.on('disconnect',  () => {
+        console.log('Client disconnected');
+    });
+
+    client.on('message', (payload) => {
+        console.log('Message received: '+payload.name)
+
+        io.emit('message', {admin: 'New message received in the server'});
+    });
+});
